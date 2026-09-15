e5bd42af14eb7b83e8cc8e50b0c417ee66f9c583
Kunj106 <141024391+Kunj106@users.noreply.github.com>
2026-09-15 19:02:42 +0530

    cors

--- src/main/java/com/kunj/SmartHomeEnergyManagementSystem/Config/WebSecurityConfig.java
+++ src/main/java/com/kunj/SmartHomeEnergyManagementSystem/Config/WebSecurityConfig.java
@@ -1,21 +1,44 @@
 package com.kunj.SmartHomeEnergyManagementSystem.Config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
-public class WebSecurityConfig
-{
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:5500",
-                "http://127.0.0.1:5500"
+                "http://127.0.0.1:5500",
+                "https://smarthomeenergymanagementsystem.onrender.com"
         ));
 
         configuration.setAllowedMethods(Arrays.asList(
@@ -23,10 +46,12 @@ public class WebSecurityConfig
                 "POST",
                 "PUT",
                 "DELETE",
+                "PATCH",
                 "OPTIONS"
         ));
 
         configuration.setAllowedHeaders(Arrays.asList("*"));
+
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
